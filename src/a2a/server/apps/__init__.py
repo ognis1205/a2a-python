b2a5676a27c3a204f076c3d30f5f11cfe53dd6cf
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-05-29 08:44:43 +0900

    fix: ensure correct agent URL path parsing and registration in Starlette app
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- src/a2a/server/apps/__init__.py
+++ src/a2a/server/apps/__init__.py
@@ -1,6 +1,10 @@
 """HTTP application components for the A2A server."""
 
-from a2a.server.apps.starlette_app import A2AStarletteApplication
+from a2a.server.apps.starlette_app import A2AStarletteApplication, A2AStarletteRouteBuilder, A2AStarletteBuilder
 
 
-__all__ = ['A2AStarletteApplication']
+__all__ = [
+    'A2AStarletteApplication',
+    'A2AStarletteRouteBuilder',
+    'A2AStarletteBuilder',
+]
